67deb23f46714aa6b9a9ae8118770ab77e341e63
IhorKhlopotin555 <hlopotinigor555@gmail.com>
2022-09-01 11:43:13 +0300

    'test'

---
+++ src/pages/home_page/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import First from "../../components/First/First";
+import Second from "../../components/Second/Second";
+
+const Home_page = () => {
+    return (
+        <div>
+            <First/>
+            <Second/>
+        </div>
+    );
+};
+
+export default Home_page;
